e5d4b6483131bd197f7d17ab379a013fb73065f7
vishwjeet thakur <ervishwjeetthakur@gmail.com>
2023-08-14 15:36:35 +0530

    updated

--- unqieType.js
+++ unqieType.js
@@ -23,93 +23,93 @@ All roman numerals answers should be provided in upper-case.
 
 //* Solution 1 
 
-// function convertToRoman(num) {
-//     let ans = "";
+function convertToRoman(num) {
+    let ans = "";
   
-//     //logic
-//     while(num>0){
-//       if(num<4){
-//         ans += "I"
-//         num--;
-//       }else if(num==4){
-//         ans += "IV";
-//         num -= 4;
-//       }else if(num >= 5 && num < 9){
-//         ans += "V";
-//         num -= 5;
-//       }else if (num==9){
-//         ans += "IX";
-//         num -= 9;
-//       }else if(num >= 10 && num < 40){
-//         ans += "X";
-//         num -= 10;
-//       }else if (num >= 40 && num < 50){
-//          ans += "XL";
-//          num -= 40;
-//       }else if(num >= 50 && num < 90){
-//         ans += "L";
-//         num -= 50;
-//       }else if(num >= 90 && num < 100){
-//         ans += "XC";
-//         num -= 90;
-//       }else if(num>=100 && num < 400){
-//         ans += "C";
-//         num -= 100;
-//       }else if(num >= 400 && num < 500){
-//         ans += "CD";
-//         num -= 400;
-//       }else if(num >= 500 && num<900){
-//         ans += "D";
-//         num -= 500;
-//       }else if(num >= 900 && num<1000){
-//         ans += "CM";
-//         num -= 900;
-//       }
+    //logic
+    while(num>0){
+      if(num<4){
+        ans += "I"
+        num--;
+      }else if(num==4){
+        ans += "IV";
+        num -= 4;
+      }else if(num >= 5 && num < 9){
+        ans += "V";
+        num -= 5;
+      }else if (num==9){
+        ans += "IX";
+        num -= 9;
+      }else if(num >= 10 && num < 40){
+        ans += "X";
+        num -= 10;
+      }else if (num >= 40 && num < 50){
+         ans += "XL";
+         num -= 40;
+      }else if(num >= 50 && num < 90){
+        ans += "L";
+        num -= 50;
+      }else if(num >= 90 && num < 100){
+        ans += "XC";
+        num -= 90;
+      }else if(num>=100 && num < 400){
+        ans += "C";
+        num -= 100;
+      }else if(num >= 400 && num < 500){
+        ans += "CD";
+        num -= 400;
+      }else if(num >= 500 && num<900){
+        ans += "D";
+        num -= 500;
+      }else if(num >= 900 && num<1000){
+        ans += "CM";
+        num -= 900;
+      }
   
-//       else if(num >= 1000){
-//         ans += "M";
-//         num -= 1000;
-//       }
-//     }
+      else if(num >= 1000){
+        ans += "M";
+        num -= 1000;
+      }
+    }
   
     
-//    return ans;
-//   }
+   return ans;
+  }
   
-//   console.log(convertToRoman(2014));
+  console.log(convertToRoman(2014));
 
 //*Solution 2 (More Efficient Approach.)
 
-// function convertToRoman(num) {
-//     const romanSymbols = [
-//       { value: 1000, symbol: 'M' },
-//       { value: 900, symbol: 'CM' },
-//       { value: 500, symbol: 'D' },
-//       { value: 400, symbol: 'CD' },
-//       { value: 100, symbol: 'C' },
-//       { value: 90, symbol: 'XC' },
-//       { value: 50, symbol: 'L' },
-//       { value: 40, symbol: 'XL' },
-//       { value: 10, symbol: 'X' },
-//       { value: 9, symbol: 'IX' },
-//       { value: 5, symbol: 'V' },
-//       { value: 4, symbol: 'IV' },
-//       { value: 1, symbol: 'I' }
-//     ];
+function convertToRoman(num) {
+    const romanSymbols = [
+      { value: 1000, symbol: 'M' },
+      { value: 900, symbol: 'CM' },
+      { value: 500, symbol: 'D' },
+      { value: 400, symbol: 'CD' },
+      { value: 100, symbol: 'C' },
+      { value: 90, symbol: 'XC' },
+      { value: 50, symbol: 'L' },
+      { value: 40, symbol: 'XL' },
+      { value: 10, symbol: 'X' },
+      { value: 9, symbol: 'IX' },
+      { value: 5, symbol: 'V' },
+      { value: 4, symbol: 'IV' },
+      { value: 1, symbol: 'I' }
+    ];
   
-//     let result = '';
+    let result = '';
   
-//     for (const symbol of romanSymbols) {
-//       while (num >= symbol.value) {
-//         result += symbol.symbol;
-//         num -= symbol.value;
-//       }
-//     }
+    for (const symbol of romanSymbols) {
+      while (num >= symbol.value) {
+        result += symbol.symbol;
+        num -= symbol.value;
+      }
+    }
   
-//     return result;
-//   }
+    return result;
+  }
   
-//   console.log(convertToRoman(2014));
+  console.log(convertToRoman(2014));
   
 //------------------------------------
 
